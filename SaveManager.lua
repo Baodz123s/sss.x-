85715768c348026eda145d5f60d7db1c917b6d92
Baodz123s <gmt2k8pkl@gmail.com>
2025-07-18 15:17:55 +0700

    Update SaveManager.lua

--- SaveManager.lua
+++ SaveManager.lua
@@ -33,7 +33,7 @@ if typeof(copyfunction) == "function" then
 end
 
 local SaveManager = {} do
-    SaveManager.Folder = "ObsidianLibSettings"
+    SaveManager.Folder = "HBPsetting"
     SaveManager.SubFolder = ""
     SaveManager.Ignore = {}
     SaveManager.Library = nil
